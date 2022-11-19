a959b02b3142348d0b81c1869438e77b618bffdd
RyunosukeEma <ee207008@meiji.ac.jp>
2022-11-20 06:42:31 +0900

    position.modelの実装

---
+++ src/db/position.model.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./connect");
+
+const Position = sequelize.define("Position", {
+  position_id: {
+    type: DataTypes.INTEGER,
+    defaultValue: 2,
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "平社員",
+    validate: {
+      isIn: [["社長", "リーダー", "平社員"]],
+    },
+  },
+});
+
+module.exports = Position;
