49639f9bcc0d3774bf1ab7a1e9931d11559aca36
RyunosukeEma <ee207008@meiji.ac.jp>
2022-11-20 07:01:28 +0900

    create testの追加

---
+++ __test__/position.test.js
@@ -0,0 +1,33 @@
+const Position = require("../src/db/position.model");
+
+describe("position test", () => {
+  beforeEach(async () => {
+    await Position.sync({ force: true });
+  });
+
+  describe("create test", () => {
+    it("ポジションの追加のテスト", async () => {
+      const position = await Position.create({
+        position_id: 2,
+        position: "平社員",
+      });
+      expect(position.position_id).toBe(2);
+      expect(position.position).toBe("平社員");
+    });
+    it("position_idのデフォルトのテスト", async () => {
+      const position = await Position.create({
+        position: "平社員",
+      });
+      expect(position.position).toBe("平社員");
+      expect(position.position_id).toBe(2);
+    });
+    it("positionのデフォルトのテスト", async () => {
+      const position = await Position.create({
+        position_id: 2,
+      });
+      expect(position.position).toBe("平社員");
+      expect(position.position_id).toBe(2);
+    });
+  });
+
+});
