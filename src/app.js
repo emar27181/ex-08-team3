592424a68e2c9ee319a46e017fe9bbf8b1daa553
soshi1429 <ee207107@meiji.ac.jp>
2022-11-19 22:32:58 +0900

    src/app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("dist/public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello team3");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
