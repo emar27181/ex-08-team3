0554a1763b3715a22ac55ae5a07bc309b3b18022
soshi1429 <ee207107@meiji.ac.jp>
2022-11-19 22:31:35 +0900

    rootに対するGETのテスト

---
+++ __test__/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../src/app");
+jest.setTimeout(20000);
+describe("Test the root path", () => {
+  it("should response the GET method", () =>
+    request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      }));
+});
