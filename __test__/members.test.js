d59345a0055affa10355260394240cfcdbbce07b
pinion9 <ee207042@meiji.ac.jp>
2022-11-20 05:55:04 +0900

    members: deleteのテストの追加

--- __test__/members.test.js
+++ __test__/members.test.js
@@ -89,4 +89,102 @@ describe("database: members table test", () => {
       expect(total).toBe(3);
     });
   });
+  describe("delete test", () => {
+    it("メンバーの削除", async () => {
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee123456",
+      });
+      await Member.destroy({ truncate: true });
+      const count = await Member.count();
+      expect(count).toBe(0);
+    });
+    it("指定したchannelの削除（チームの解散）", async () => {
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee234567",
+      });
+      await Member.create({
+        channel_id: 2,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee345678",
+      });
+      await Member.destroy({
+        where: {
+          channel_id: 1,
+        },
+      });
+      const members = await Member.findAll();
+      const total = await Member.count();
+      expect(members[0].employee_id).toBe("ee123456");
+      expect(total).toBe(1);
+    });
+    it("memberをすべてのchannelから削除（退社）", async () => {
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 2,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 2,
+        employee_id: "ee234567",
+      });
+      await Member.create({
+        channel_id: 3,
+        employee_id: "ee123456",
+      });
+      await Member.destroy({
+        where: {
+          employee_id: "ee123456",
+        },
+      });
+      const members = await Member.findAll();
+      const total = await Member.count();
+      expect(members[0].channel_id).toBe(2);
+      expect(total).toBe(1);
+    });
+    it("channelから特定のemployeeを削除（開発チームからの脱退）", async () => {
+      await Member.create({
+        channel_id: 1,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 2,
+        employee_id: "ee123456",
+      });
+      await Member.create({
+        channel_id: 2,
+        employee_id: "ee234567",
+      });
+      await Member.create({
+        channel_id: 3,
+        employee_id: "ee123456",
+      });
+      await Member.destroy({
+        where: {
+          channel_id: 2,
+          employee_id: "ee123456",
+        },
+      });
+      const members = await Member.findAll({
+        where: {
+          employee_id: "ee123456",
+        },
+      });
+      const total = await members.length;
+      expect(members[0].channel_id).toBe(1);
+      expect(members[1].channel_id).toBe(3);
+      expect(total).toBe(2);
+    });
+  });
 });
