8dc4de6788006c5d767645c5e01d672c6ae1130b
soshi1429 <ee207107@meiji.ac.jp>
2022-11-22 21:03:49 +0900

    login.controllerの修正

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const loginRouter = express.Router();
 
 loginRouter.get("/", (req, res) => {
-    res.status(418).end();
+  res.render("login", {});
 });
 
 module.exports = loginRouter;
