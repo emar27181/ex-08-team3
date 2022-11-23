e797023d3b9e56633ad82ccef51951a1333b9a25
RyunosukeEma <ee207008@meiji.ac.jp>
2022-11-23 21:19:47 +0900

    channels.controllerの修正(all.ejsの追加に関する変更)

--- src/channels/channels.controller.js
+++ src/channels/channels.controller.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const channels = require("./channels.model");
+const all = require("./all.model");
 const channelsRouter = express.Router();
 
 channelsRouter.get("/", channels.redirectToAll);
+channelsRouter.get("/all", all.displayAll);
 
 module.exports = channelsRouter;
