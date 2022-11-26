80aa61e8118567074a3fa230782fd1fb64e05f1b
RyunosukeEma <ee207008@meiji.ac.jp>
2022-11-26 14:33:21 +0900

    model/messageから情報を取得

--- src/channels/all.model.js
+++ src/channels/all.model.js
@@ -1,6 +1,22 @@
+const Message = require("../db/model/message");
+
 const allModel = {
-  displayAll: (req, res) => {
-    res.render("all", {});
+  displayAll: async (req, res) => {
+    await Message.sync();
+    const messages = await Message.findAll();
+    const formatedMessages = [];
+    for (const message of messages) {
+      const formatedMessage = {
+        content: message.content,
+        time: message.time,
+        message_id: message.message_id,
+        channel_id: message.channel_id,
+      };
+      formatedMessages.push(formatedMessage);
+    }
+    res.render("all", {
+      messages: formatedMessages,
+    });
   },
 };
 
