67640541391259b2bf5be4781fb28945067439cc
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2022-11-28 00:24:25 +0900

    fixし忘れ

--- public/index.js
+++ public/index.js
@@ -3,5 +3,5 @@ const openSidebar = require("./src/openSidebar");
 const button = document.getElementById("sidebar-disp");
 
 button.addEventListener("click", () => {
-    openSidebar();
+  openSidebar();
 });
