087149a21d4f4f1edb30bfb8ba4b00587241eadf
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2022-11-27 22:39:47 +0900

    asideのクラス変更関数の中身を実装

--- public/src/openSidebar.js
+++ public/src/openSidebar.js
@@ -1,3 +1,11 @@
-const openSidebar = () => null;
+const openSidebar = () => {
+  const sidebar = document.querySelector("aside");
+
+  if (sidebar.getAttribute("class") === "open") {
+    sidebar.setAttribute("class", "close");
+  } else {
+    sidebar.setAttribute("class", "open");
+  }
+};
 
 module.exports = openSidebar;
