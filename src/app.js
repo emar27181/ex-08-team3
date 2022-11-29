c85cc786c394eb6e3457b4b01aba61ad6620978c
soshi1429 <ee207107@meiji.ac.jp>
2022-11-29 13:29:29 +0900

    ルーターの登録

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const app = express();
-// const authRouter = require("./auth/controller");
+const authRouter = require("./auth/controller");
 const channelsRouter = require("./channels/channels.controller");
 
 app.set("view engine", "ejs");
@@ -21,6 +21,8 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
   })
 );
+
+app.use("/", authRouter);
 
 app.use("/channels", channelsRouter);
 app.get("/", (req, res) => {
