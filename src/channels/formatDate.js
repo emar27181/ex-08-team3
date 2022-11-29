83b38a93cfd6677f3f13cae84c15df4c1f66dcb6
soshi1429 <ee207107@meiji.ac.jp>
2022-11-30 02:05:27 +0900

    formatの修正

--- src/channels/formatDate.js
+++ src/channels/formatDate.js
@@ -1,17 +1,21 @@
 const formatDate = function (date) {
-  const formatDateOptions = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "2-digit",
-    second: "2-digit",
-    weekday: "short",
-  };
-
-  return new Intl.DateTimeFormat("ja", formatDateOptions).format(
-    new Date(date + (date.getTimezoneOffset() + 9 * 60) * 60 * 1000)
+  const now = new Date();
+  const jpDate = new Date(
+    date + (date.getTimezoneOffset() + 9 * 60) * 60 * 1000
   );
+  const year = jpDate.getFullYear();
+  const day = jpDate.getDate();
+  let formatedDate = `${jpDate.getHours()}:${jpDate
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+  if (now.getDate() !== day) {
+    formatedDate = `${jpDate.getMonth() + 1}/${day} ${formatedDate}`;
+  }
+  if (now.getFullYear() !== year) {
+    formatedDate = `${year}/${formatedDate}`;
+  }
+  return formatedDate;
 };
 
 module.exports = formatDate;
