816d67d15ef40356daa2e6bc61c3c541fb3dd1d5
soshi1429 <ee207107@meiji.ac.jp>
2022-11-29 16:06:20 +0900

    formatする日付をJSTにする

--- src/channels/formatDate.js
+++ src/channels/formatDate.js
@@ -8,7 +8,10 @@ const formatDate = function (date) {
     second: "2-digit",
     weekday: "short",
   };
-  return new Intl.DateTimeFormat("ja", formatDateOptions).format(date);
+
+  return new Intl.DateTimeFormat("ja", formatDateOptions).format(
+    new Date(date + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000)
+  );
 };
 
 module.exports = formatDate;
