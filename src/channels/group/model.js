91db21234054959aeaf8a7b42f8834cbd92b3bc1
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2022-12-11 17:26:50 +0900

    groupページにメッセージとチャンネル情報を送る

--- src/channels/group/model.js
+++ src/channels/group/model.js
@@ -1,6 +1,28 @@
+const Channel = require("../../db/model/channnel");
+const Message = require("../../db/model/message");
+const formatDate = require("../formatDate");
+
 const groupModel = {
-  displayGruopPage: (req, res) => {
-    res.render("group", {});
+  displayGruopPage: async (req, res) => {
+    const channels = await Channel.findAll();
+    const messages = await Message.findAll({
+      where: { channel_id: 3 },
+    });
+    const formatedMessages = [];
+    for (const message of messages) {
+      const formatedMessage = {
+        content: message.content,
+        time: formatDate(message.time),
+        message_id: message.message_id,
+        channel_id: message.channel_id,
+        employee_id: message.employee_id,
+      };
+      formatedMessages.push(formatedMessage);
+    }
+    res.render("group", {
+      channels,
+      messages: formatedMessages,
+    });
   },
 };
 
