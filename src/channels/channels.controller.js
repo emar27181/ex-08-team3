c69317f38bbf294c0ccaf9e85d17c4e65e85564d
Ka-plus <ee207033@meiji.ac.jp>
2022-12-14 19:08:52 +0900

    接続エラーの修正

--- src/channels/channels.controller.js
+++ src/channels/channels.controller.js
@@ -9,7 +9,7 @@ channelsRouter.get("/", channels.redirectToAll);
 channelsRouter.get("/all", all.displayMessage);
 channelsRouter.post("/all", all.addMessage);
 channelsRouter.get("/me", me.displayDM);
-channelsRouter.post("/me", me.addMessage);
+//channelsRouter.post("/me", me.addMessage);
 
 channelsRouter.use("/group", groupRouter);
 
