65c29b2286e2ccda12061103dbbd678c407729d0
Ka-plus <ee207033@meiji.ac.jp>
2022-12-20 16:03:53 +0900

    コメントの修正

--- public/src/all.js
+++ public/src/all.js
@@ -2,11 +2,9 @@ const openSidebar = require("./openSidebar");
 
 const contentBody = document.getElementById("content-body");
 const button = document.getElementById("sidebar-disp");
-// const Employee = require("../../src/channels/all.model");
 
 contentBody.scrollIntoView(false);
 
 button.addEventListener("click", () => {
   openSidebar();
 });
-
