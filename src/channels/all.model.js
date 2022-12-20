c002a1309e41fb321829a1eef80c7aa5ae7b7cba
Ka-plus <ee207033@meiji.ac.jp>
2022-12-20 22:33:37 +0900

    社員管理画面のリスト

--- src/channels/all.model.js
+++ src/channels/all.model.js
@@ -37,9 +37,21 @@ const allModel = {
     });
     await Message.sync();
     const channels = await Channel.findAll();
+    const employees = await Employee.findAll();
+    const formatedEmployees = [];
+    for (const employee of employees) {
+      const formatedEmployee = {
+        employee_id: employee.employee_id,
+        name: employee.name,
+        password: employee.passwoed,
+        position_id: employee.position_id,
+      };
+      formatedEmployees.push(formatedEmployee);
+    }
     res.render("admin", {
       user,
       channels,
+      employees: formatedEmployees,
     });
   },
 
