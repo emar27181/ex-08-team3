641275d5f003d5547357eaf6b351d592b9fcae88
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2022-12-22 13:51:16 +0900

    画面最下にスクロールする機能を切り出し

---
+++ public/messageCh.js
@@ -0,0 +1,2 @@
+const contentBody = document.getElementById("content-body");
+contentBody.scrollIntoView(false);
--- public/src/all.js
+++ public/src/all.js
@@ -1,9 +1,6 @@
 const openSidebar = require("./openSidebar");
 
-const contentBody = document.getElementById("content-body");
 const button = document.getElementById("sidebar-disp");
-
-contentBody.scrollIntoView(false);
 
 button.addEventListener("click", () => {
   openSidebar();
