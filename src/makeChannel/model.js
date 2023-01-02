d6cfd437f227413d43bee18e63315db2235433ef
pinion9 <ee207042@meiji.ac.jp>
2023-01-02 14:12:47 +0900

    チャンネル作成画面のメニューバーに使うデータベースの受け渡し

--- src/makeChannel/model.js
+++ src/makeChannel/model.js
@@ -1,8 +1,16 @@
 const Channel = require("../db/model/channnel");
+const Employee = require("../db/model/employee");
 
 const makeChannelModel = {
-  renderToChannel: (req, res) => {
-    res.render("channel", {});
+  renderToChannel: async (req, res) => {
+    const user = await Employee.findOne({
+      where: { employee_id: req.session.id },
+    });
+    const channels = await Channel.findAll();
+    res.render("channel", {
+      user,
+      channels,
+    });
   },
   addChannel: async (req, res) => {
     const reqData = req.body;
