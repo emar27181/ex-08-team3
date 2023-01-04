dec5493f352c8209734f783015e973baba1ee59e
Ka-plus <ee207033@meiji.ac.jp>
2023-01-05 01:59:26 +0900

    POST処理

--- src/auth/controller.js
+++ src/auth/controller.js
@@ -4,6 +4,7 @@ const Employee = require("../db/model/employee");
 const all = require("../channels/all.model.js");
 const admin = require("../channels/admin.model.js");
 const mypage = require("../channels/mypage.model.js");
+const gadmin = require("../channels/group_admin.model.js");
 
 authRouter.get("/login", (req, res) => {
   res.render("login", {});
@@ -12,6 +13,7 @@ authRouter.get("/login", (req, res) => {
 authRouter.get("/admin", all.displayAdmin);
 authRouter.get("/admin/involve", admin.displayInvolve);
 authRouter.get("/mypage", mypage.displayMypage);
+authRouter.get("/group_admin", gadmin.displayGAdmin);
 authRouter.post("/admin/involve", admin.addMember);
 authRouter.post("/admin", admin.editMember);
 authRouter.post("/mypage", mypage.editMe);
