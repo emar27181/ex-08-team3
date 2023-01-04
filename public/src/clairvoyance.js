cf439fd1d165fc3bd124fc6ffcdfc9e957c6957c
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2023-01-05 05:24:30 +0900

    パスワード確認の可不可を変更する関数の実装

--- public/src/clairvoyance.js
+++ public/src/clairvoyance.js
@@ -1,3 +1,14 @@
-const clairvoyance = (eye) => false;
+const clairvoyance = (eye) => {
+  const passwordInput = document.getElementById("employee-password");
+  const inputType = passwordInput.getAttribute("type");
+
+  if (inputType === "password") {
+    passwordInput.setAttribute("type", "text");
+    eye.setAttribute("class", "bi bi-eye");
+  } else {
+    passwordInput.setAttribute("type", "password");
+    eye.setAttribute("class", "bi bi-eye-fill");
+  }
+};
 
 module.exports = clairvoyance;
