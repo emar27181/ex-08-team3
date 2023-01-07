55dfe466a55c24baace225f871c18b1528b95064
soshi1429 <ee207107@meiji.ac.jp>
2023-01-08 04:02:18 +0900

    channels/allへのリクエストの修正

--- src/channels/all.model.js
+++ src/channels/all.model.js
@@ -1,32 +1,27 @@
-const Channel = require("../db/model/channnel");
-const Employee = require("../db/model/employee");
-const Message = require("../db/model/message");
-const Member = require("../db/model/member");
+const { Employee, GroupEmployees, Group, AllMessage } = require("../db/model");
 const formatDate = require("./formatDate");
 const matchMyId = require("./matchMyId");
 
 const allModel = {
   displayMessage: async (req, res) => {
     const user = await Employee.findOne({
-      where: { employee_id: req.session.id },
+      where: { id: req.session.id },
     });
-    const channelsJoin = await Member.findAll({
-      where: { employee_id: req.session.id },
+    const channelsJoin = await GroupEmployees.findAll({
+      where: { EmployeeId: req.session.id },
     });
-    await Message.sync();
-    const channels = await Channel.findAll();
-    const messages = await Message.findAll({
-      where: { channel_id: 1 },
-    });
+    await AllMessage.sync();
+    const channels = await Group.findAll();
+    const messages = await AllMessage.findAll();
     const formatedMessages = [];
     for (const message of messages) {
       const formatedMessage = {
         content: message.content,
-        time: formatDate(message.time),
-        message_id: message.message_id,
-        channel_id: message.channel_id,
-        employee_id: message.employee_id,
-        who: matchMyId(message.employee_id, req.session.id),
+        time: formatDate(message.createdAt),
+        message_id: message.id,
+        channel_id: message.GroupId,
+        employee_id: message.Employee.name,
+        who: matchMyId(message.EmployeeId, req.session.id),
       };
       formatedMessages.push(formatedMessage);
     }
@@ -41,31 +36,30 @@ const allModel = {
   addMessage: async (req, res) => {
     const reqData = req.body;
 
-    await Message.create({
+    await AllMessage.create({
       content: reqData.content,
-      channel_id: 1,
-      employee_id: req.session.id,
+      EmployeeId: req.session.id,
     });
     res.redirect("/channels/all");
   },
 
   displayAdmin: async (req, res) => {
     const user = await Employee.findOne({
-      where: { employee_id: req.session.id },
+      where: { id: req.session.id },
     });
-    const channelsJoin = await Member.findAll({
-      where: { employee_id: req.session.id },
+    const channelsJoin = await GroupEmployees.findAll({
+      where: { id: req.session.id },
     });
-    await Message.sync();
-    const channels = await Channel.findAll();
+    await AllMessage.sync();
+    const channels = await Group.findAll();
     const employees = await Employee.findAll();
     const formatedEmployees = [];
     for (const employee of employees) {
       const formatedEmployee = {
-        employee_id: employee.employee_id,
+        employee_id: employee.id,
         name: employee.name,
         password: employee.password,
-        position_id: employee.position_id,
+        position_id: employee.PositionId,
       };
       formatedEmployees.push(formatedEmployee);
     }
