3041e74d527de5064620383d139d7545da5c63e4
soshi1429 <ee207107@meiji.ac.jp>
2023-01-07 22:51:03 +0900

    positionテーブルの修正

---
+++ src/db/model.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./connect");
+
+const Position = sequelize.define("Position", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    defaultValue: 3,
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "平社員",
+    validate: {
+      isIn: [["社長", "リーダー", "平社員"]],
+    },
+  },
+});
+
+module.exports = {
+  Position,
+};
