dc7e0e540fc455672623587a35c97f467ddbc2b5
soshi1429 <ee207107@meiji.ac.jp>
2023-01-08 01:37:31 +0900

    初期データの修正

--- src/db/initialize.js
+++ src/db/initialize.js
@@ -1,15 +1,21 @@
-const Channel = require("./model/channnel");
-const Employee = require("./model/employee");
-const Member = require("./model/member");
-const Message = require("./model/message");
-const Position = require("./model/position");
+const {
+  Position,
+  Employee,
+  GroupEmployees,
+  Group,
+  AllMessage,
+  GroupMessage,
+  DirectMessage,
+} = require("./model");
 const createInitialData = require("./initialData");
 
 (async () => {
-  await Channel.sync({ force: true });
-  await Employee.sync({ force: true });
-  await Member.sync({ force: true });
-  await Message.sync({ force: true });
   await Position.sync({ force: true });
+  await Employee.sync({ force: true });
+  await AllMessage.sync({ force: true });
+  await Group.sync({ force: true });
+  await GroupEmployees.sync({ force: true });
+  await GroupMessage.sync({ force: true });
+  await DirectMessage.sync({ force: true });
   await createInitialData();
 })();
