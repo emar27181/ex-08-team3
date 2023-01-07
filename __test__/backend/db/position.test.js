5863858a26ca2c1479065c81fff4fd49c55061f4
soshi1429 <ee207107@meiji.ac.jp>
2023-01-07 22:51:38 +0900

    positionテーブルのテストの修正

--- __test__/backend/db/position.test.js
+++ __test__/backend/db/position.test.js
@@ -1,4 +1,4 @@
-const Position = require("../../../src/db/model/position");
+const { Position } = require("../../../src/db/model");
 
 describe("position test", () => {
   beforeEach(async () => {
@@ -8,36 +8,36 @@ describe("position test", () => {
   describe("create test", () => {
     it("ポジションの追加のテスト", async () => {
       const position = await Position.create({
-        position_id: 2,
+        id: 2,
         position: "平社員",
       });
-      expect(position.position_id).toBe(2);
+      expect(position.id).toBe(2);
       expect(position.position).toBe("平社員");
     });
-    it("position_idのデフォルトのテスト", async () => {
+    it("idのデフォルトのテスト", async () => {
       const position = await Position.create({
         position: "平社員",
       });
       expect(position.position).toBe("平社員");
-      expect(position.position_id).toBe(3);
+      expect(position.id).toBe(3);
     });
     it("positionのデフォルトのテスト", async () => {
       const position = await Position.create({
-        position_id: 2,
+        id: 2,
       });
       expect(position.position).toBe("平社員");
-      expect(position.position_id).toBe(2);
+      expect(position.id).toBe(2);
     });
   });
 
   describe("find test", () => {
     it("ポジションの取り出し", async () => {
       await Position.create({
-        position_id: 2,
+        id: 2,
         position: "平社員",
       });
       const positions = await Position.findAll();
-      expect(positions[0].position_id).toBe(2);
+      expect(positions[0].id).toBe(2);
       expect(positions[0].position).toBe("平社員");
     });
   });
