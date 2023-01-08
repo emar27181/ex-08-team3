76015d9aa72ecbcf883c0343feefff53fe43a511
SoftwarePrac2022-b8 <ee207079@meiji.ac.jp>
2023-01-08 14:48:10 +0900

    個人向けメッセージ画面を表示できるように修正

--- src/channels/me.model.js
+++ src/channels/me.model.js
@@ -1,33 +1,23 @@
-const Channel = require("../db/model/channnel");
-const Employee = require("../db/model/employee");
-const Member = require("../db/model/member");
+const { Employee, GroupEmployees, Group } = require("../db/model");
 
 const meModel = {
   displayDM: async (req, res) => {
     const user = await Employee.findOne({
-      where: { employee_id: req.session.id },
+      where: { id: req.session.id },
     });
-    const channelsJoin = await Member.findAll({
-      where: { employee_id: req.session.id },
+    const channelsJoin = await GroupEmployees.findAll({
+      where: { EmployeeId: req.session.id },
     });
-    const channels = await Channel.findAll();
+    const channels = await Group.findAll();
     await Employee.sync();
-    const employees = await Employee.findAll();
-    const formatedEmployees = [];
-    for (const employee of employees) {
-      const formatedEmployee = {
-        employee_id: employee.employee_id,
-        name: employee.name,
-        password: employee.passwoed,
-        position_id: employee.position_id,
-      };
-      formatedEmployees.push(formatedEmployee);
-    }
+    const employees = await Employee.findAll({
+      attributes: ["id", "name", "PositionId"],
+    });
     res.render("me", {
       user,
       channelsJoin,
       channels,
-      employees: formatedEmployees,
+      employees,
     });
   },
 };
